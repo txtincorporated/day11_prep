ceadc7124bad11b2d51e9902444aadc47c8eca6d
Nathan P. Keene <npk@txtincorporated.com>
2016-06-22 02:09:30 -0700

    Done

--- app.js
+++ app.js
@@ -7,21 +7,28 @@ function setRouteMappings() {
   page('about', about);
   page('contact', contact);
   page('contact/:contactName', contact);
+  page('Nathan', Nathan);
+  page('Fashnizzle', Fashnizzle);
+  page('Fashnizzle', Fashizzle);
   page('*', notfound); // catch-all
 
   page();
 }
 
-function index() { p.textContent = 'viewing index (home)'; }
-function about() { p.textContent = 'viewing about'; }
+// function index() { p.textContent = 'viewing index (home)'; }
+function index() {$('p').text('viewing index (home)');}
+function about() {$('p').text('viewing about');}
 function contact(ctx) {
-  p.textContent = 'viewing contact ' + (ctx.params.contactName || '');
+  $('p').text('viewing contact ' + (ctx.params.contactName || ''));
 }
+function Nathan() {$('p').text('viewing Nathan');}
+function Fashnizzle() {$('p').text('viewing Fashnizzle');}
+function Fashizzle() {$('p').text('viewing Fashizzle');}
 function notfound() {
-  p.innerHTML = '<h3>OMG!</h3><p>The page at "'
+  $('p').html('<h3>OMG!</h3><p>The page at "'
               + location.hostname + location.pathname + location.search
               + '" can\'t be found (like a bridge over troubled water, ya\' know)!</p>'
-              + '<img src="lost.jpg"></img>';
+              + '<img src="lost.jpg"></img>');
 }
 
 setRouteMappings();
